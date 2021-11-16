13bda17955ea2802db19d0dee2ff0775757650fb
abhishekgupta23 <53884462+abhishekgupta23@users.noreply.github.com>
2021-11-16 20:35:10 +0530

    Update Translator.py

--- Translator.py
+++ Translator.py
@@ -1,1 +1,17 @@
+import streamlit as st
+from textblob import TextBlob
+import pandas as pd
 
+st.title ("Converter")
+a=st.text_input("enter the text you want to convert")
+c=['af','sq','am','ar','hy','az','eu','be','bn','bs','bg','ca','ceb','zh','zh-TW','co','hr','cs','da','nl','en','eo','et','fi','fr','fy','gl','ka','de','el','gu','ht','ha','haw','iw','hi','hmn','hu','is','ig','id','ga','it','ja','jv','kn','kk','km','rw','ko','ku','ky','lo','la','lv','lt','lb','mk','mg','ms','ml','mt','mi','mr','mn','my','ne','no','ny','or','ps','fa','pl','pt','pa','ro','ru','sm','gd','sr','st','sn','sd','si','sk','sl','so','es','su','sw','sv','tl','tg','ta','tt','te','th','tr','tk','uk','ur','ug','uz','vi','cy','xh','yi','yo','zu']
+if a:
+    selected_box = st.selectbox(
+                'Choose one of the following',
+                ('Afrikaans','Albanian','Amharic','Arabic','Armenian','Azerbaijani','Basque','Belarusian','Bengali','Bosnian','Bulgarian','Catalan','Cebuano','Chinese (Simplified)','Chinese (Traditional)','Corsican','Croatian','Czech','Danish','Dutch','English','Esperanto','Estonian', 'Finnish', 'French','Frisian','Galician','Georgian','German','Greek','Gujarati','Haitian Creole','Hausa','Hawaiian','Hebrew','Hindi','Hmong', 'Hungarian','Icelandic','Igbo','Indonesian','Irish','Italian','Japanese', 'Javanese','Kannada','Kazakh','Khmer','Kinyarwanda', 'Korean', 'Kurdish', 'Kyrgyz', 'Lao', 'Latin', 'Latvian', 'Lithuanian', 'Luxembourgish', 'Macedonian', 'Malagasy', 'Malay', 'Malayalam', 'Maltese', 'Maori', 'Marathi', 'Mongolian', 'Myanmar (Burmese)', 'Nepali', 'Norwegian', 'Nyanja (Chichewa)', 'Odia (Oriya)', 'Pashto', 'Persian','Polish', 'Portuguese (Portugal, Brazil)', 'Punjabi', 'Romanian', 'Russian', 'Samoan', 'Scots Gaelic', 'Serbian','Sesotho', 'Shona', 'Sindhi', 'Sinhala (Sinhalese)', 'Slovak', 'Slovenian', 'Somali', 'Spanish','Sundanese', 'Swahili', 'Swedish', 'Tagalog (Filipino)', 'Tajik', 'Tamil', 'Tatar', 'Telugu', 'Thai', 'Turkish', 'Turkmen', 'Ukrainian', 'Urdu', 'Uyghur', 'Uzbek', 'Vietnamese', 'Welsh', 'Xhosa', 'Yiddish', 'Yoruba', 'Zulu')
+                )
+    int d = selected_box.index()
+    blob = TextBlob(a)
+    g = blob.translate(to=c[d])
+    st.write("translated text :-")
+    st.write(g)
